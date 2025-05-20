b4011c53a011ef2161e690ea17ea6b1576b535da
Kurra Shiva <2003A51091@sru.edu.in>
2025-05-20 17:51:30 +0530

    Initial commit with my own credentials

---
+++ src/pages/projectsdata.js
@@ -0,0 +1,46 @@
+const projectsdata = [
+  {
+    title: 'TrendCart',
+    image: './cart.svg',
+    description:
+      'TrendCart User-friendly e-commerce platform. Secure, stylish, diverse products. Engaging design, intuitive experience. Top fashion shopping destination.',
+    link: '',
+  },
+  {
+    title: 'PitchPerfector',
+    image: './ground.svg',
+    description:
+      'PitchPerfector Streamline ground rentals with an intuitive map feature. Easily discover and explore rental options in desired locations.',
+    link: 'https://github.com/KurraShiva/PitchPerfector',
+  },
+  {
+    title: 'TechSolutionsHub',
+    image: './TechSolutionsHub.svg',
+    description:
+      'TechSolutionsHub Interactive tech forum for engineers. Share, solve, and collaborate on tech challenges. Empowering the open-source community.',
+    link: '',
+  },
+  {
+    title: 'GitHub-Insight',
+    image: './GitHub-Insight.svg',
+    description:
+      'GitHub Insight: Responsive UI, comprehensive GitHub profiles, real-time data, and tech analysis. Access developer insights effortlessly.',
+    link: '',
+  },
+  {
+    title: 'sofashop-ecomm',
+    image: './sofashop-ecomm.svg',
+    description:
+      'SofaShop-Ecomm: User-friendly e-commerce for sofas and home decor. Shop online effortlessly for stylish furniture and decor items.',
+    link: '',
+  },
+  {
+    title: 'Reddit-like',
+    image: './Reddit-like.svg',
+    description:
+      'Reddit-Like: A Reddit-inspired web app. Users post, comment, upvote, downvote, edit, and create subreddits. Engage in dynamic discussions and content sharing.',
+    link: '',
+  },
+]
+
+export default projectsdata
